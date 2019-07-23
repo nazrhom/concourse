62853c95ce676f4960e1fc40f91487416e1cb67a
Joshua Winters <jwinters@pivotal.io>
2019-07-23 10:58:40 -0400

    atc: update checker interval
    Signed-off-by: Joshua Winters <jwinters@pivotal.io>
    Co-authored-by: Rui Yang <ryang@pivotal.io>

--- atc/atccmd/command.go
+++ atc/atccmd/command.go
@@ -570,7 +570,6 @@ func (cmd *RunCommand) constructAPIMembers(
 	workerClient := worker.NewClient(pool, workerProvider)
 
 	checker := resourceserver.NewChecker(
-		logger,
 		secretManager,
 		checkFactory,
 		cmd.GlobalResourceCheckTimeout,
@@ -805,7 +804,7 @@ func (cmd *RunCommand) constructBackendMembers(
 				dbCheckFactory,
 				engine,
 			),
-			10*time.Second,
+			10*time.Minute,
 			bus,
 		)},
 		{Name: "pipelines", Runner: pipelines.SyncRunner{
